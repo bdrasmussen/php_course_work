d2d612a038efc0e172833deeb3ef04e340e6f1f3
bdrasmussen <rasmub18@gmail.com>
2015-10-01 01:36:47 +0000

    First shot at error detection

--- project1/Project1.php
+++ project1/Project1.php
@@ -1,4 +1,52 @@
+<?php
 
+if (isset($_POST['submit']))
+{
+
+    $noun = $_POST['noun'];
+    $verb = $_POST['verb'];
+    $adj = $_POST['adj'];
+    $adverb = $_POST['adverb'];
+    $output_form = false;
+
+    $dbc = mysqli_connect('localhost', 'bdrasmussen', 'root', 'manlibs')
+     or die('Error connecting to MySQL server.');
+  
+
+
+  if (empty($noun) && empty($verb) && empty($adj) && empty($adverb))
+  {
+   echo 'Fill in a field <br />';
+   $output_form = true;
+  }
+  
+}
+else
+{
+  $output_form = true;
+}
+    
+
+  if((!empty($noun)) && (!empty($verb)) && (!empty($adj)) && (!empty($adverb)))
+  {
+    $query = "INSERT INTO madlibs (noun, verb, adj, adverb)  VALUES ('$noun', '$verb', '$adj', '$adverb')";
+    mysqli_query($dbc, $query)
+      or die('Error querying database.');
+    $output_form = true;
+  } 
+    $query1 = "SELECT * FROM madlibs";
+    $result = mysqli_query($dbc, $query1);
+    while ($row = mysqli_fetch_array($result)) 
+    {
+      echo 'Do you ' . $row['verb'] . ' your ' . $row['adj'] . ' ' . $row['noun'] . ' ' . $row['adverb'] . "? That's hilarious!";
+      echo '<br />';
+    }
+
+  mysqli_close($dbc);
+  
+   if ($output_form)
+  {
+?>
 <body>
 
   <p>Play Mad-libs!</p>
@@ -14,44 +62,8 @@
     <input type="submit" name="submit" value="Submit" />
     
   </form>
-
-
 <?php
-    $noun = $_POST['noun'];
-    $verb = $_POST['verb'];
-    $adj = $_POST['adj'];
-    $adverb = $_POST['adverb'];
-    $output_form = false;
-
-     $dbc = mysqli_connect('localhost', 'bdrasmussen', 'root', 'manlibs')
-       or die('Error connecting to MySQL server.');
-    
-
-  if((!empty($noun)) && (!empty($verb)) && (!empty($adj)) && (!empty($adverb)))
-  {
-    $query = "INSERT INTO madlibs (noun, verb, adj, adverb)  VALUES ('$noun', '$verb', '$adj', '$adverb')";
-    mysqli_query($dbc, $query)
-      or die('Error querying database.');
-    
-  } 
-    $query1 = "SELECT * FROM madlibs";
-    $result = mysqli_query($dbc, $query1);
-    while ($row = mysqli_fetch_array($result)) 
-    {
-      echo 'Do you ' . $row['verb'] . ' your ' . $row['adj'] . ' ' . $row['noun'] . ' ' . $row['adverb'] . "? That's hilarious!";
-     /* echo $row['noun'];
-      echo ' ' . $row['verb'];
-      echo ' ' . $row['adj'];
-      echo ' ' . $row['adverb']; */
-      echo '<br />';
-    }
-
-
-
-
-
-
-  mysqli_close($dbc);
+  }
 ?>
 
 
