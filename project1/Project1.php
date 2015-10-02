bb25c7af57440363c3675add3e9db838d17f522d
bdrasmussen <rasmub18@gmail.com>
2015-10-02 01:03:53 +0000

    reverse order

--- project1/Project1.php
+++ project1/Project1.php
@@ -1,23 +1,41 @@
 <?php
 
-if (isset($_POST['submit']))
-{
 
     $noun = $_POST['noun'];
     $verb = $_POST['verb'];
     $adj = $_POST['adj'];
     $adverb = $_POST['adverb'];
     $output_form = false;
-
     $dbc = mysqli_connect('localhost', 'bdrasmussen', 'root', 'manlibs')
      or die('Error connecting to MySQL server.');
-  
-
-
+ 
+if (isset($_POST['submit']))
+{
   if (empty($noun) && empty($verb) && empty($adj) && empty($adverb))
   {
-   echo 'Fill in a field <br />';
+   echo 'All of your fields are empty, please fill in all fields. <br />';
    $output_form = true;
+  }
+  
+  else if (empty($noun))
+  {
+    echo 'Fill in the noun field';
+    $output_form = true;
+  }
+  else if (empty($verb))
+  {
+    echo 'Fill in the verb field';
+    $output_form = true;
+  }
+  else if (empty($adj))
+  {
+    echo 'Fill in the adjective field';
+    $output_form = true;
+  }
+  else if (empty($adverb))
+  {
+    echo 'Fill in the adverb field';
+    $output_form = true;
   }
   
 }
@@ -26,26 +44,8 @@ else
   $output_form = true;
 }
     
-
-  if((!empty($noun)) && (!empty($verb)) && (!empty($adj)) && (!empty($adverb)))
-  {
-    $query = "INSERT INTO madlibs (noun, verb, adj, adverb)  VALUES ('$noun', '$verb', '$adj', '$adverb')";
-    mysqli_query($dbc, $query)
-      or die('Error querying database.');
-    $output_form = true;
-  } 
-    $query1 = "SELECT * FROM madlibs";
-    $result = mysqli_query($dbc, $query1);
-    while ($row = mysqli_fetch_array($result)) 
-    {
-      echo 'Do you ' . $row['verb'] . ' your ' . $row['adj'] . ' ' . $row['noun'] . ' ' . $row['adverb'] . "? That's hilarious!";
-      echo '<br />';
-    }
-
-  mysqli_close($dbc);
-  
-   if ($output_form)
-  {
+if ($output_form)
+{
 ?>
 <body>
 
@@ -63,7 +63,30 @@ else
     
   </form>
 <?php
-  }
+}
+  if((!empty($noun)) && (!empty($verb)) && (!empty($adj)) && (!empty($adverb)))
+  {
+    $query = "insert into madlibs(madlib_sk, noun, verb, adj, adverb) values ((select count(*) + 1 from madlibs),'$noun', '$verb', '$adj', '$adverb');";
+    
+    mysqli_query($dbc, $query)
+      or die('Error querying database.');
+    $output_form = true;
+    
+  } 
+    $query1 = "select * from madlibs order by madlib_sk desc;";
+    $result = mysqli_query($dbc, $query1);
+    $arraycount = 0;
+    
+    while ($row = mysqli_fetch_array($result)) 
+    {
+      echo 'Do you ' . $row['verb'] . ' your ' . $row['adj'] . ' ' . $row['noun'] . ' ' . $row['adverb'] . "? That's hilarious!";
+      echo '<br />';      
+      $arraycount++;
+    }
+
+  mysqli_close($dbc);
+  
+    
 ?>
 
 
